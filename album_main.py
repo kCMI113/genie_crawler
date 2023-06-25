d197e492c85f1d77ac08e9b7b5eb763ea7800a88
Dongyeon Park <yeon0729@sookmyung.ac.kr>
2023-06-26 01:30:48 +0900

    refactor: change album img crawling method #12

--- album_main.py
+++ album_main.py
@@ -14,14 +14,14 @@ from src.utils import resizeImg
 @hydra.main(version_base="1.2", config_path="configs", config_name="config.yaml")
 def main(config: DictConfig = None) -> None:
     file_path = config.out_dir
-    playlist_file = "playlists.csv"
-    song_file = "songs.csv"
+    playlist_file = config.pl_concat
+    song_file = config.song_concat
 
     pl_df = pd.read_csv(os.path.join(file_path, playlist_file))
     song_df = pd.read_csv(os.path.join(file_path, song_file))
 
-    non_album_ids = list(song_df[song_df["IMG_PATH"].apply(lambda x: not bool(re.search(".png|.JPG|.PNG|.jpg", x)))]["ALBUM_ID"])
-    album_url = "https://www.genie.co.kr/detail/albumInfo?axnm="
+    non_album_ids = list(set(song_df[song_df["IMG_PATH"].apply(lambda x: not bool(re.search(".png|.JPG|.PNG|.jpg", x)))]["ALBUM_ID"]))
+    album_url = config.album_url
 
     album_img_list = []
     for id in tqdm(non_album_ids):
@@ -35,7 +35,9 @@ def main(config: DictConfig = None) -> None:
 
         details = driver.find_element(By.CLASS_NAME, "album-detail-infos")
         photo = details.find_element(By.CLASS_NAME, "photo-zone")
-        img_path = (photo.find_element(By.TAG_NAME, "a")).get_attribute("href")
+        img = (photo.find_element(By.CLASS_NAME, "thum")).find_element(By.TAG_NAME, "a")
+
+        img_path = img.get_attribute("href")
         resized_img_path = resizeImg(img_path[: img_path.find("/dims")], config.img_resize, config.max_resize)
 
         info = {
@@ -49,7 +51,7 @@ def main(config: DictConfig = None) -> None:
 
     album_img_df = pd.DataFrame(album_img_list)
 
-    new_album_img_path = os.path.join(file_path, "new_album_imgs.csv")
+    new_album_img_path = os.path.join(file_path, config.album_img_filename)
     album_img_df.to_csv(new_album_img_path, index=False)
 
 
