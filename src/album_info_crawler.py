75a2911ce22a1fad0551ec70c7ecddff2fef3ebf
2jun0 <soo28819@gmail.com>
2023-07-06 14:50:26 +0000

    IMR-24 feat : replace selenium to requests

--- src/album_info_crawler.py
+++ src/album_info_crawler.py
@@ -1,25 +1,21 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from datetime import date
 from logging import Logger
+from bs4 import BeautifulSoup
+import requests
 
 
 def crawlAlbumInfo(album_id: int, album_detail_url: str, log: Logger) -> dict:
-    op = webdriver.ChromeOptions()
-    op.add_argument("--headless")
-    op.add_argument("--no-sandbox")
-
-    driver = webdriver.Chrome(options=op)
-
     url = f"{album_detail_url}{album_id}"
-    driver.get(url=url)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"}
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
 
     log.info("- Crawling Album Release Date : %d", album_id)
 
-    album_detail_info = driver.find_element(By.CLASS_NAME, "album-detail-infos")
+    album_detail_info = soup.select_one(".album-detail-infos")
 
-    info_zone = album_detail_info.find_element(By.CLASS_NAME, "info-zone")
-    date_info = info_zone.find_elements(By.TAG_NAME, "li")[-1].text
+    info_zone = album_detail_info.select_one(".info-zone")
+    date_info = info_zone.select("li")[-1].text
     date_info = list(map(int, date_info.split(".")))
     date_info = date(date_info[0], date_info[1], date_info[2])
     log.info("RELEASE_DATE: %s", date_info.strftime("%Y-%m-%d"))
@@ -32,7 +28,5 @@ def crawlAlbumInfo(album_id: int, album_detail_url: str, log: Logger) -> dict:
     log.info("ALBUM_IMG_PATH: %s", resized_img_path)
 
     """
-
-    driver.close()
 
     return {"ALBUM_ID": album_id, "RELEASE_DATE": date_info}
