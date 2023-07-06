06e0dd999c2a8ce8b68b68ab030505f3bc93a504
kCMI113 <cmkang1316@gmail.com>
2023-07-06 14:43:31 +0900

    IMR-20 feat: add img crawling and change some structure

--- album_date_main.py
+++ album_date_main.py
@@ -4,9 +4,8 @@ from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 from omegaconf import DictConfig
 import os
-
 from src.utils import getLogger
-from src.song_detail_crawler import crawlSongDetail
+from src.album_date_crawler import crawlAlbumInfo
 
 
 @hydra.main(version_base="1.2", config_path="configs", config_name="album_date.yaml")
@@ -18,26 +17,30 @@ def main(config: DictConfig = None) -> None:
     # load base song info
     song_info_path = os.path.join(config.out_dir, config.song_info_filename)
     log.info("Load song_info files to retrieve song_id from %s", song_info_path)
-    song_info_df = pd.read_csv(song_info_path, dtype={"SONG_ID": int})
-    album_ids: list[int] = song_info_df["ALBUM_ID"].unique().to_list()
+    song_info_df = pd.read_csv(song_info_path, dtype={"ALBUM_ID": int})
+    album_ids: list[int] = song_info_df["ALBUM_ID"].unique().tolist()
 
     # crawling playlists and songs
-    log.info("Start album date crawling ...")
-    album_dates = []
+    log.info("Start album info crawling ...")
+    album_infos = []
     failed_crawling_album_ids = []
-    for song_id in tqdm(album_ids, position=0, leave=True):
+    for album_id in tqdm(album_ids, position=0, leave=True):
+        print(album_id)
         with logging_redirect_tqdm():
             try:
-                date_info = crawlSongDetail(song_id, config.song_detail_url, log)
-                album_dates.append(date_info)
+                album_info = crawlAlbumInfo(album_id, config.album_detail_url, log, config)
+                album_infos.append(album_info)
             except Exception as e:
                 log.exception(e)
-                failed_crawling_album_ids.append(song_id)
+                failed_crawling_album_ids.append(album_id)
 
     if failed_crawling_album_ids:
         log.warn("Failed crawling song ids : %s", str(failed_crawling_album_ids))
 
-    print(album_dates)
+    csv_path = os.path.join(config.out_dir, config.album_info_filename)
+    log.info("Save album_infos to %s", csv_path)
+    album_infos_df = pd.DataFrame(album_infos)
+    album_infos_df.to_csv(csv_path, index=False)
 
 
 if __name__ == "__main__":
