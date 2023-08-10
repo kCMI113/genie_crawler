03640824d1ad30daf8051d9a6d0e0f8c3492a842
kCMI113 <cmkang1316@gmail.com>
2023-08-10 12:10:22 +0900

    IMR-160 refactor: Album to album

--- db/repository/album.py
+++ db/repository/album.py
@@ -7,27 +7,27 @@ from mongoengine import QuerySet
 
 class AlbumRepository:
     def create_Album(self, genie_id: str, name: str, img_url: str, released_date: date, created_at: datetime) -> Album:
-        Album = AlbumDocument(
+        album = AlbumDocument(
             genie_id=genie_id, name=name, img_url=img_url, released_date=released_date, created_at=created_at, updated_at=created_at
         )
-        saved: AlbumDocument = Album.save()
+        saved: AlbumDocument = album.save()
         return saved.to_dto()
 
     def delete_by_genie_id(self, genie_id: str) -> None:
-        Album: AlbumDocument = AlbumDocument.objects(genie_id=genie_id)
+        album: AlbumDocument = AlbumDocument.objects(genie_id=genie_id)
 
-        if not Album:
+        if not album:
             raise NotFoundAlbumException(f"Can't find Album document: genie_id={genie_id}")
 
-        Album.delete()
+        album.delete()
 
     def find_by_genie_id(self, genie_id: str) -> Album:
-        Album: AlbumDocument = AlbumDocument.objects(genie_id=genie_id).first()
+        album: AlbumDocument = AlbumDocument.objects(genie_id=genie_id).first()
 
-        if not Album:
+        if not album:
             return None
 
-        return Album.to_dto()
+        return album.to_dto()
 
     def find_by_updated_at_gte(self, query_dt: datetime) -> list[Album]:
         albums: QuerySet[AlbumDocument] = AlbumDocument.objects(upated_at__gte=query_dt)
@@ -38,5 +38,5 @@ class AlbumRepository:
         return [album.to_dto() for album in albums]
 
     def find_all(self) -> list[Album]:
-        Albums: QuerySet[AlbumDocument] = AlbumDocument.objects
-        return [Album.to_dto() for Album in Albums]
+        albums: QuerySet[AlbumDocument] = AlbumDocument.objects
+        return [album.to_dto() for album in albums]
