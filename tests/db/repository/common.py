63af7b95efae396e629c7d3a76469ddbb64a10a3
kCMI113 <cmkang1316@gmail.com>
2023-08-11 15:23:45 +0900

    IMR-163 fix: change db to db_name

--- tests/db/repository/common.py
+++ tests/db/repository/common.py
@@ -1,18 +1,18 @@
 import dotenv
 import os
-import db
 import mongomock
+import db
 
 
 def connect_to_db():
     dotenv.load_dotenv()
     host = os.environ.get("DB_HOST")
-    db = os.environ.get("DB_NAME")
+    db_name = os.environ.get("DB_NAME")
     username = os.environ.get("DB_USERNAME")
     password = os.environ.get("DB_PASSWORD")
 
     db.connect(
-        db=db,
+        db=db_name,
         host=host,
         username=username,
         password=password,
