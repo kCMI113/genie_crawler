63c6f6390e7839378a96baa1002946d7e6413840
kCMI113 <cmkang1316@gmail.com>
2023-08-13 13:29:09 +0900

    IMR-157 feat:  add album migration module

--- migration/song.py
+++ migration/song.py
@@ -16,19 +16,29 @@ class SongCsvMigrate:
         self.song_repository = SongRepository()
 
     def add_albums(self) -> None:
-        # df에서 앨범관련 컬럼만 살려서 반복문 돌며 디비에 넣기
-        return
+        print("##### MIGRATING ALBUM TO DB #####")
+        album_df = self.df[["ALBUM_TITLE", "ALBUM_ID", "IMG_PATH", "RELEASE_DATE"]]
+
+        for idx in tqdm(range(0, len(album_df))):
+            name = album_df.iloc[idx]["ALBUM_TITLE"]
+            id = str(album_df.iloc[idx]["ALBUM_ID"])
+            img_url = album_df.iloc[idx]["IMG_PATH"]
+            released_date = album_df.iloc[idx]["RELEASE_DATE"]
+
+            if not self.album_repository.find_by_genie_id(id):
+                self.album_repository.create_Album(id, name, img_url, released_date)
 
     def find_album(self, genie_id: str) -> Album:
-        # 지니아이디로 쿼리(지니아이디)로 들어온 앨범 도쿠먼트 찾아서 보내기
         return
 
     def add_artists(self) -> None:
         print("##### MIGRATING ARTIST TO DB #####")
         artist_df = self.df[["ARTIST_NAME", "ARTIST_ID"]]
+
         for idx in tqdm(range(0, len(artist_df))):
             name = artist_df.iloc[idx]["ARTIST_NAME"]
             id = str(artist_df.iloc[idx]["ARTIST_ID"])
+
             if not self.artist_repository.find_by_genie_id(id):
                 self.artist_repository.create_artist(id, name)
 
