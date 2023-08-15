43efa6625658c70d4ee6487371fa1db9f19bed0a
kCMI113 <cmkang1316@gmail.com>
2023-08-15 08:25:03 +0000

    IMR-172 feat: add genie config for crawler option

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings
-
+from datetime import datetime
 
 class DBConfig(BaseSettings):
     spotify_cid: str
@@ -10,7 +10,14 @@ class DBConfig(BaseSettings):
     db_password: str
     input_path: str = "migration/input"
     pl_file: str = "playlists_0721.csv"
-    song_file: str = "join_songs_0726.csv"
+    song_file: str = "join_songs_0726_unique.csv"
+    query_date: datetime = datetime.utcnow()
 
     class Config:
         env_file = ".env"
+
+class GenieConfig(BaseSettings):
+    start_idx: int = 1
+    query_date: datetime = datetime.utcnow()
+    img_resize: int = 244
+    max_resize: int = 600
