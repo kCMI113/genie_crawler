153af8b208dd720658d29d1c511564124e3205e6
kCMI113 <cmkang1316@gmail.com>
2023-08-16 12:15:51 +0900

    IMR-173 refactor: remove db

--- db/document/__init__.py
+++
@@ -1,6 +0,0 @@
-from .album import AlbumDocument
-from .artist import ArtistDocument
-from .playlist import PlaylistDocument
-from .song import SongDocument
-
-__all__ = [AlbumDocument, ArtistDocument, PlaylistDocument, SongDocument]
--- db/repository/__init__.py
+++
@@ -1,6 +0,0 @@
-from .album import AlbumRepository
-from .artist import ArtistRepository
-from .playlist import PlaylistRepository
-from .song import SongRepository
-
-__all__ = [AlbumRepository, ArtistRepository, PlaylistRepository, SongRepository]
